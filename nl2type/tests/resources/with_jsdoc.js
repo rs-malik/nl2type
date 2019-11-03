8f2826656d911b5a8452bff09da330d6617c095c
rabee <sohail.rabee@gmail.com>
2019-11-03 13:43:00 +0100

    Added test for jsdoc extraction

---
+++ nl2type/tests/resources/with_jsdoc.js
@@ -0,0 +1,10 @@
+/**
+ * Adds two numbers together
+ *
+ * @param {number} first The First Number
+ * @param {number} second The Second Number
+ * @returns {number}
+ */
+function add(first, second) {
+  return first + second;
+}
---
+++ nl2type/tests/test_extraction.py
@@ -0,0 +1,53 @@
+from nl2type import extract
+
+
+def test_jsdoc_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    assert jsdoc_output is not None
+    functions = [out for out in jsdoc_output if extract.is_function_signature(out)]
+    assert len(functions) == 1
+
+
+def test_name_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    func = [out for out in jsdoc_output if extract.is_function_signature(out)][0]
+    assert func['name'] == 'add'
+
+
+def test_params_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    func = [out for out in jsdoc_output if extract.is_function_signature(out)][0]
+    assert len(func['params']) == 2
+
+
+def test_param_names_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    func = [out for out in jsdoc_output if extract.is_function_signature(out)][0]
+    params = func['params']
+    param_first = [param for param in params if param['name'] == 'first']
+    assert len(param_first) == 1
+    param_second = [param for param in params if param['name'] == 'second']
+    assert len(param_second) == 1
+
+
+def test_param_types_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    params = [out for out in jsdoc_output if extract.is_function_signature(out)][0]['params']
+    param_first = [param for param in params if param['name'] == 'first'][0]
+    assert len(param_first['type']['names']) == 1
+    assert param_first['type']['names'][0] == 'number'
+
+
+def test_param_comments_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    params = [out for out in jsdoc_output if extract.is_function_signature(out)][0]['params']
+    param_first = [param for param in params if param['name'] == 'first'][0]
+    assert param_first['description'] == 'The First Number'
+
+
+def test_return_comment_extracted():
+    jsdoc_output = extract.extract_from_file("resources/with_jsdoc.js")
+    func = [out for out in jsdoc_output if extract.is_function_signature(out)][0]
+    assert len(func['returns']) == 1
+    assert len(func['returns'][0]['type']['names']) == 1
+    assert func['returns'][0]['type']['names'][0] == 'number'
